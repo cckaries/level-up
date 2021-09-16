b943aabae0a6ade2689fb23c079516b2f822e698
Shou-Chi Chang <cckaries@gmail.com>
2021-09-16 22:25:55 +0800

    feat(main): add class component demo

--- src/components/Main/ClassMain.js
+++ src/components/Main/ClassMain.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
 
 export class ClassMain extends Component {
+  /* old
+    constructor(props) {
+      super(props);
+      this.state = { 
+        state1: 'abc',
+        state2: 'def'
+      };
+    }
+  */
+
+  // new
+  state = {
+    state1: 'abc',
+    state2: 'def',
+  }
+
   componentDidMount() {
     // do something when a component is mounted
   }
 
   static getDerivedStateFromProps(props, state) {
-    // when a component is about to update
+    // when a component is about to mount or update
   }
 
   componentDidUpdate(prevProps, prevState) {
     // when a component is updated
+    this.setState({state1: 'xyz', state2: '123'});
   }
   
   componentWillMount() {
