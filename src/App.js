f715dc6b09eb2b25c8141ef99f4620730913f891
Shou-Chi Chang <cckaries@gmail.com>
2021-09-23 01:13:51 +0800

    feat(app): implement increment w 'useDispatch'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import styles from './App.module.scss';
 
 function App() {
+  const dispatch = useDispatch();
   const reduxCount = useSelector(state => state.count);
 
   console.log('reduxCount:', reduxCount);
 
   return (
     <div className={styles.Container}>
+      <button onClick={() => dispatch({ type: 'increment' })}>ADD</button>
       <Header />
       <Main />
     </div>
