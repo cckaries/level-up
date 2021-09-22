240afcba12184b652e228da76be7b701aa169e13
Shou-Chi Chang <cckaries@gmail.com>
2021-09-23 01:04:05 +0800

    feat(app): get redux state w 'useSelector'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { useSelector } from 'react-redux';
+
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import styles from './App.module.scss';
 
 function App() {
+  const reduxCount = useSelector(state => state.count);
+
+  console.log('reduxCount:', reduxCount);
+
   return (
     <div className={styles.Container}>
       <Header />
