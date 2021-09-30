8f5cc8d83b77e63beb60ca4b055695c3acef7d46
Shou-Chi Chang <cckaries@gmail.com>
2021-09-30 20:00:24 +0800

    feat(index): export actions

--- src/store/index.js
+++ src/store/index.js
@@ -63,4 +63,6 @@ const store = configureStore({
 });
 
 export const counterActions = counterSlice.actions;
+export const textActions = textSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
