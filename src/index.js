12c26bbd0b6d3ce781d976f09ddbdd2548e58995
Shou-Chi Chang <cckaries@gmail.com>
2021-09-30 19:44:17 +0800

    refactor(index): separate store from index file

--- src/index.js
+++ src/index.js
@@ -2,46 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 import App from './App';
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: { count: 0, text: 'here shows what you type stored in REDUX' },
-  reducers: {
-    increment(state) {
-      state.count++;
-    },
-    decrement(state) {
-      state.count--;
-    },
-    setText(state, payload) {
-      state.text = payload;
-    },
-  },
-});
-
-// const counterReducer = (
-//   state = { count: 0, text: 'here shows what you type stored in REDUX' },
-//   action = { type: null, payload: null }
-// ) => {
-//   switch (action.type) {
-//     case 'INCREMENT':
-//       return { ...state, count: state.count + 1 };
-//     case 'DECREMENT':
-//       return { ...state, count: state.count - 1 };
-//     case 'SET_TEXT':
-//       return { ...state, text: action.payload || '' };
-//     default:
-//       return state;
-//   }
-// };
-
-// const store = createStore(counterSlice.reducer);
-const store = configureStore({ reducer: counterSlice.reducer });
+import store from './store/';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+// import { createStore } from 'redux';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: { count: 0, text: 'here shows what you type stored in REDUX' },
+  reducers: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    },
+    setText(state, payload) {
+      state.text = payload;
+    },
+  },
+});
+
+// const counterReducer = (
+//   state = { count: 0, text: 'here shows what you type stored in REDUX' },
+//   action = { type: null, payload: null }
+// ) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return { ...state, count: state.count + 1 };
+//     case 'DECREMENT':
+//       return { ...state, count: state.count - 1 };
+//     case 'SET_TEXT':
+//       return { ...state, text: action.payload || '' };
+//     default:
+//       return state;
+//   }
+// };
+
+// const store = createStore(counterSlice.reducer);
+const store = configureStore({ reducer: counterSlice.reducer });
+
+export default store;
