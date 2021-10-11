76e5d6104cd3be9f0e6a5fd3cd50cd103970cac1
Shou-Chi Chang <cckaries@gmail.com>
2021-10-11 22:29:56 +0800

    feat(store): add 'sentences'

--- src/store/text.js
+++ src/store/text.js
@@ -2,10 +2,13 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const textSlice = createSlice({
   name: 'text',
-  initialState: { text: 'here shows what you type stored in REDUX' },
+  initialState: { text: 'here shows what you type stored in REDUX', sentences: {} },
   reducers: {
     setText(state, action) {
       state.text = action.payload;
+    },
+    setSentences(state, action) {
+      state.sentences = action.payload;
     },
   },
 });
