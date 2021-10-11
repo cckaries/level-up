b61c8aafe8858db7b57d59e553f7ea3694ca1bb1
Shou-Chi Chang <cckaries@gmail.com>
2021-10-11 22:57:06 +0800

    feat(store): add 'putTextThunk'

--- src/store/text.js
+++ src/store/text.js
@@ -13,5 +13,29 @@ const textSlice = createSlice({
   },
 });
 
+export const putTextThunk = text => {
+  // return {type: '', payload: ...}}
+  return dispatch => {
+    /* 
+      1. call the api using PUT method to send the text to firebase db
+      2. fetch list items
+      3. display list items in <Right />
+    */
+    fetch('https://level-f5322-default-rtdb.firebaseio.com/text.json', {
+      method: 'PUT',
+      body: JSON.stringify({
+        text,
+      }),
+    }).then(() =>
+      fetch('https://level-f5322-default-rtdb.firebaseio.com/list.json')
+        .then(res => res.json())
+        .then(resJson => {
+          console.log(resJson);
+          dispatch(textActions.setSentences(resJson));
+        })
+    );
+  };
+};
+
 export const textActions = textSlice.actions;
 export default textSlice.reducer;
