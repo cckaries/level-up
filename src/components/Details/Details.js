566037b7681a0625c8a7e6eca82962462dc30a9c
Shou-Chi Chang <cckaries@gmail.com>
2021-10-19 23:51:51 +0800

    feat(pages): add 'DetailsPage'

---
+++ src/components/Details/Details.js
@@ -0,0 +1,13 @@
+// import { useEffect, useReducer } from 'react';
+
+import styles from './Details.module.scss';
+
+const Details = props => {
+  return (
+    <div id="details-container" className={styles.Container}>
+      <h1>this is details page</h1>
+    </div>
+  );
+};
+
+export default Details;
---
+++ src/pages/details.js
@@ -0,0 +1,7 @@
+import Details from '../components/Details/Details';
+
+const DetailsPage = () => {
+  return <Details />;
+};
+
+export default DetailsPage;
