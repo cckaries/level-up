92b34d25857df6d6de926fed08ba672529ba1899
Shou-Chi Chang <cckaries@gmail.com>
2021-10-19 23:11:19 +0800

    feat(pages): add 'dictionary'

---
+++ src/components/Dictionary/Dictionary.js
@@ -0,0 +1,13 @@
+import { useEffect, useReducer } from 'react';
+
+import styles from './Dictionary.module.scss';
+
+const Dictionary = props => {
+  return (
+    <div id="dictionary-container" className={styles.Container}>
+      <h1>this is dictionary page</h1>
+    </div>
+  );
+};
+
+export default Dictionary;
