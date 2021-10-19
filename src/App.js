442343ac43ba0d09457c41e535ba55e27a9564cb
Shou-Chi Chang <cckaries@gmail.com>
2021-10-19 23:04:21 +0800

    refactor(app): use 'MainPage' instead of 'Main' component directly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch, useSelector } from 'react-redux';
 
 import styles from './App.module.scss';
 import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import { counterActions } from './store/counter';
+import MainPage from './pages/main';
+// import { counterActions } from './store/counter';
 
 function App() {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+  // const count = useSelector(state => state.counter.count);
+  // const dispatch = useDispatch();
 
   return (
     <div className={styles.Container}>
-      <button onClick={() => dispatch(counterActions.increment())}>ADD</button>
+      {/* <button onClick={() => dispatch(counterActions.increment())}>ADD</button>
       <button onClick={() => dispatch(counterActions.decrement())}>
         REDUCE
       </button>
-      <div>{count}</div>
+      <div>{count}</div> */}
       <Header />
-      <Main />
+      <MainPage />
     </div>
   );
 }
