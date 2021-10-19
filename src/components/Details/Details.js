8debde2ac6840201beace2840b3b8d52588fbeb9
Shou-Chi Chang <cckaries@gmail.com>
2021-10-19 23:58:33 +0800

    feat(details): show 'sentenceId' in details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,11 +1,16 @@
 // import { useEffect, useReducer } from 'react';
 
+import { useParams } from 'react-router-dom';
 import styles from './Details.module.scss';
 
 const Details = props => {
+  const params = useParams();
+
+  console.log('params:', params);
+
   return (
     <div id="details-container" className={styles.Container}>
-      <h1>this is details page</h1>
+      <h1>this is details page {params.sentenceId}</h1>
     </div>
   );
 };
