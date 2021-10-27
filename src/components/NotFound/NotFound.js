1cdbd76307c6b1230df084eb1d55c077b62e60fc
Shou-Chi Chang <cckaries@gmail.com>
2021-10-28 00:32:37 +0800

    feat(not-found): add redirect interval & use 'useHistory' hook

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import styles from './NotFound.module.scss';
 
 const NotFound = props => {
+  const history = useHistory();
+  const [timerSec, setTimerSec] = useState(5);
+
+  const redirectPage = () => {
+    history.push('/');
+  };
+
+  useEffect(() => {
+    if (!timerSec) return redirectPage();
+
+    const redirectInterval =
+      !!timerSec &&
+      setInterval(() => {
+        setTimerSec(timerSec - 1);
+      }, 1000);
+
+    return () => clearInterval(redirectInterval);
+  }, [timerSec]);
+
   return (
     <div id="details-container" className={styles.Container}>
       <h1>Not Found</h1>
+      <div>will redirect to main page in {timerSec} secs.</div>
     </div>
   );
 };
